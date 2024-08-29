ed671d231c7331ab9ce92df18676a2695b828410
MahoganyGant <mahoganygant02@gmail.com>
2024-08-29 14:56:42 -0400

    my first java homework example

---
+++ src/main/java/demo/Playground.java
@@ -0,0 +1,9 @@
+package demo;
+//example done with Mikaila
+public class Playground {
+    public static void main(String[] args) {
+
+        System.out.println("Mahogany");
+
+    }
+}
---
+++ src/main/java/personal_demo/Practice.java
@@ -0,0 +1,8 @@
+package personal_demo;
+
+public class Practice {
+    public static void main(String[] args) {
+
+        System.out.println("Let's get this Coding Money!");
+    }
+}
